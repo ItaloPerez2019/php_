ac2dcb07108c083271f064656b7ea5eebcac4828
Italo E Perez <italoeperez@italos-mbp-2.myfiosgateway.com>
2020-12-29 14:05:46 -0500

    migration issue && foreign key

---
+++ app-php/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Attempting;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+
+
+class RegisterController extends Controller
+{
+
+    public function __construct(){
+        $this->middleware(['guest']);
+
+    }
+
+    public function index(){
+        $message="hello";
+        return view('auth.register');
+    }
+
+
+    public function store(Request $request)
+
+    {
+
+        $this -> validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|max:255',
+            'password' => 'required|confirmed',
+        ]);
+
+
+       $user= User::create([
+            'name' => $request -> name,
+            'username' => $request -> username,
+            'email' => $request -> email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->attempt($request->only('email', 'password'));
+
+
+        // auth()->Auth::attempt($request->only('email', 'password'));
+
+
+            return redirect ()->route('dashboard',['user'=>$user]);
+
+        }
+    }
+
